a76c30c61ea43cabd3c5f3def6b0d00739ea9ea2
saijayanth59 <sithlord5995@gmail.com>
2024-05-02 11:11:27 +0530

    Change route

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
 app.get("/", (req, res) => {
-  res.render("/", { name: "", bmi: 0 });
+  res.render("index", { name: "", bmi: 0 });
 });
 
 app.post("/", (req, res) => {
