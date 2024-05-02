9e97f5c992d7a38e350b231f7b8e9574a0c17d63
saijayanth59 <sithlord5995@gmail.com>
2024-05-02 10:57:47 +0530

    Intial Commit for chocolate

---
+++ index.js
@@ -0,0 +1,27 @@
+const express = require("express");
+const path = require("path");
+const app = express();
+const bodyParser = require("body-parser");
+
+app.set("view engine", "ejs");
+app.set(express.static(path.join(__dirname, "views")));
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+app.get("/home", (req, res) => {
+  res.render("index", { name: "", bmi: 0 });
+});
+
+app.post("/home", (req, res) => {
+  weight = parseFloat(req.body.weight);
+  height = parseFloat(req.body.height);
+  result = weight / (height * height);
+  result;
+
+  res.render("index", { name: req.body.name, bmi: result });
+});
+
+app.listen(2222, () => {
+  console.log("listening,,");
+});
